e95d7a72efb9e26b119b50996a022b5d4069aa6f
unknown <aungshome@outlook.com>
2023-09-19 08:03:32 +0700

    Fix ESLint

--- app/page.tsx
+++ app/page.tsx
@@ -24,11 +24,11 @@ export default function Home() {
 		<div className="flex flex-col items-center justify-center">
 			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 				<div className="inline-block max-w-2xl text-center justify-center">
-					<h1 className={title()}>Hi, I'm&nbsp;</h1>
+					<h1 className={title()}>Hi, I&apos;m&nbsp;</h1>
 					<h1 className={title({ color: "blue" })}><span className="font-black">AungS8430</span></h1>
 					<br />
 					<h1 className={title()}>
-						I'm a <span className={title({ color: "blue" })}>
+						I&apos;m a <span className={title({ color: "blue" })}>
 							<Typewriter
 								words={["Coder", "Web Developer", "Backend Developer", "Freelancer", "Designer"]}
 								loop={0}
@@ -41,7 +41,7 @@ export default function Home() {
 						</span>
 					</h1>
 					<h2 className={subtitle({ class: "mt-5" })}>
-						I love coding cool stuffs. I'm learning new things everyday!
+						I love coding cool stuffs. I&apos;m learning new things everyday!
 					</h2>
 				</div>
 				<div className="flex gap-3">
@@ -63,7 +63,7 @@ export default function Home() {
 				<Image src="/about.png" width={1200} height={1200} alt="Cover" className="hidden sm:flex" />
 				<section className="flex flex-col items-start justify-start text-start gap-4">
 					<h1 className={title()}>About Me?!</h1>
-					<p>Hi! You may, or may not know my name(I guess not!) but you can call me AungS8430. I'm currently a student at Suankularb Wittayalai School. I'm 14 years old, I started coding when I was 11, because I wanted to code a Discord bot, I guess I've come so far! If you wanna ask questions or just wanna talk, please don't hesitate to reach me out!</p>
+					<p>Hi! You may, or may not know my name(I guess not!) but you can call me AungS8430. I&apos;m currently a student at Suankularb Wittayalai School. I&apos;m 14 years old, I started coding when I was 11, because I wanted to code a Discord bot, I guess I&apos;ve come so far! If you wanna ask questions or just wanna talk, please don&apos;t hesitate to reach me out!</p>
 				</section>
 			</section>
 			<Divider className="my-4" />
@@ -76,7 +76,7 @@ export default function Home() {
 								<Card>
 									<CardBody>
 										<h1 className="text-2xl font-bold">2021-Present</h1>
-										<p>Currently studying GATE Program at Suankularb Wittayalai School, mostly focused on subjects like Mathematics, Sciences, etc. But I prefer Computer class. I'm also taking Harvard's CS50x at the moment.</p>
+										<p>Currently studying GATE Program at Suankularb Wittayalai School, mostly focused on subjects like Mathematics, Sciences, etc. But I prefer Computer class. I&apos;m also taking Harvard&apos;s CS50x at the moment.</p>
 									</CardBody>
 								</Card>
 							</Tab>
@@ -84,7 +84,7 @@ export default function Home() {
 								<Card>
 									<CardBody>
 										<h1 className="text-2xl font-bold">2011-2015</h1>
-										<p>I had been studying in Saint Gabriel's College for primary school, I learnt a lot of stuffs back there, I love the I.E. classes so much that I still miss them even after I moved out in 2021.</p>
+										<p>I had been studying in Saint Gabriel&apos;s College for primary school, I learnt a lot of stuffs back there, I love the I.E. classes so much that I still miss them even after I moved out in 2021.</p>
 									</CardBody>
 								</Card>
 							</Tab>
