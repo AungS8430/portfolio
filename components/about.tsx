6e3d33156c6f472b76ceb5e1f0d6c5c36da47c68
aungs8430 <aungshome@outlook.com>
2026-03-28 13:44:06 +0700

    Changed the sticky behavior in the about section

--- components/about.tsx
+++ components/about.tsx
@@ -6,7 +6,7 @@ export default function About({ scrollY, containerHeight }: { scrollY: MotionVal
   return (
     <div className="flex flex-col h-full">
       <h3 className="sticky top-14 sm:top-10 md:top-12 lg:top-20 ml-auto w-fit text-right font-sans text-xl">About Me</h3>
-      <p className="font-sans text-2xl md:text-3xl lg:text-5xl sm:leading-8 md:leading-10 lg:leading-14 max-w-3xl text-balance mt-24 sm:mt-16 lg:mt-28">
+      <p className="sticky top-44 sm:top-36 lg:top-48 font-sans text-2xl md:text-3xl lg:text-5xl sm:leading-8 md:leading-10 lg:leading-14 max-w-3xl text-balance">
         I&#39;m a Web Developer and Computer Engineering student at KOSEN-KMITL. I’m passionate about building functional web applications.
       </p>
     </div>
