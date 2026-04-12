46a58fd48cf348bc0efc674524cc560499d641f2
aungs8430 <aungshome@outlook.com>
2026-04-12 19:38:23 +0900

    Updated about

--- components/about.tsx
+++ components/about.tsx
@@ -7,7 +7,7 @@ export default function About({ scrollY, containerHeight }: { scrollY: MotionVal
     <div className="flex flex-col h-full pb-24">
       <h3 className="sticky top-14 sm:top-10 md:top-12 lg:top-20 ml-auto w-fit text-right font-sans text-xl">About Me</h3>
       <p className="sticky top-44 sm:top-36 lg:top-48 font-sans text-2xl md:text-3xl lg:text-5xl sm:leading-8 md:leading-10 lg:leading-14 max-w-3xl text-balance">
-        I&#39;m a Web Developer and Computer Engineering student at KOSEN-KMITL. I’m passionate about building functional web applications.
+        I&#39;m a Web Developer and a student at KOSEN-KMITL. I’m passionate about building web applications and other tools.
       </p>
     </div>
   )
